f3eb51a4d9c89b90c160ac823578f0815f65c7e3
YUK5COB <K.Yuvaraj@in.bosch.com>
2024-07-06 21:51:58 +0530

    new test case

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -12,10 +12,15 @@ TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits)
   EXPECT_EQ(2, soundex[1]);
   EXPECT_EQ(3, soundex[2]);
   EXPECT_EQ(4, soundex[3]);
+
+}
+
+TEST(SoudexTestsuite, Checktwovowels)
+{
 // to check two vowels case
   soundex[5] = {};
-  const char* name1 = "Tymczak";
-  generateSoundex(name1, soundex);
+  const char* name = "Tymczak";
+  generateSoundex(name, soundex);
   EXPECT_EQ('T', soundex[0]);
   EXPECT_EQ(5, soundex[1]);
   EXPECT_EQ(2, soundex[2]);
