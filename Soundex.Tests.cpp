0fd62d409b2b97b08b6cf29d7ae5abde4f215c32
YUK5COB <K.Yuvaraj@in.bosch.com>
2024-07-06 21:30:23 +0530

    integer update

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -8,7 +8,7 @@ TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
   const char* name = "AXDL";
   generateSoundex(name, soundex);
   EXPECT_EQ('A', soundex[0]);
-  EXPECT_EQ('2', soundex[1]);
-  EXPECT_EQ('3', soundex[2]);
-  EXPECT_EQ('4', soundex[3]);
+  EXPECT_EQ(2, soundex[1]);
+  EXPECT_EQ(3, soundex[2]);
+  EXPECT_EQ(4, soundex[3]);
 }
